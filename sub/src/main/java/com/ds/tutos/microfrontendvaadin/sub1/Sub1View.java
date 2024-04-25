109a3808cd44072774679c1d52e68c5717048f9a
Didier SIMON <didier_simon@goodyear.com>
2024-04-25 22:06:28 +0200

    added cors config

--- sub/src/main/java/com/ds/tutos/microfrontendvaadin/sub1/Sub1View.java
+++ sub/src/main/java/com/ds/tutos/microfrontendvaadin/sub1/Sub1View.java
@@ -5,7 +5,7 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
-@Route("")
+//@Route("")
 public class Sub1View extends Div {
 
     public Sub1View() {
